fc77a89160e7d92a736c18804f8c7c09f1b2bb3c
JulieJulie <jtatica2002@gmail.com>
2024-08-08 00:29:55 -0500

    [update] nuxtconfig import theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import Lara from "@primevue/themes/Lara";
+import Lara from "@primevue/themes/lara";
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
